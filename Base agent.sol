bcfa596868dd52fd2fef9d5d5229f1f1c91bda0a
MoodyFullWatts <Mockmoody@gmail.com>
2025-06-20 20:02:13 -1000

    Update Base agent.sol
    Adding owner access control correcting syntx.

--- Base agent.sol
+++ Base agent.sol
@@ -2,21 +2,33 @@ pragma solidity 0.8.0;
 
 contract BaseAgent {
     string public agentName;
+    address public owner;
+    mapping(uint => string) public agentData;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
 
     constructor(string memory name) {
+        owner = msg.sender;
         agentName = name;
     }
 
     function updateName(string memory newName) public {
         agentName = newName;
     }
-function createNewAgent(string memory newAgentName) public returns (address) {
-    BaseAgent newAgent = new BaseAgent(newAgentName);
-    return address(newAgent);
-mapping(uint => string) public agentData;
 
-function storeAgentData(uint id, string memory data) public {
-    agentData[id] = data;
-}function callTool(string memory toolName, string memory input) public returns (string memory) {
-    return string(abi.encodePacked("Calling ", toolName, " with ", input));
-}}
+    function createNewAgent(string memory newAgentName) public returns (address) {
+        BaseAgent newAgent = new BaseAgent(newAgentName);
+        return address(newAgent);
+    }
+
+    function storeAgentData(uint id, string memory data) public {
+        agentData[id] = data;
+    }
+
+    function callTool(string memory toolName, string memory input) public returns (string memory) {
+        return string(abi.encodePacked("Calling ", toolName, " with ", input));
+    }
+}
