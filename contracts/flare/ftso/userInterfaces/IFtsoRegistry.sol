6e14380ba224895b52ca3141077e4a0b66f206ea
MoodyFullWatts <Mockmoody@gmail.com>
2025-06-29 01:24:38 -1000

    Update IFtsoRegistry.sol
    get ftso by symbol

--- contracts/flare/ftso/userInterfaces/IFtsoRegistry.sol
+++ contracts/flare/ftso/userInterfaces/IFtsoRegistry.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6;
 
-interface IFlareContractRegistry {
-    function getContractAddressByName(string calldata _name) external view returns (address);
-    function getContractAddressByHash(bytes32 _nameHash) external view returns (address);
-    function getContractAddressesByHash(bytes32[] calldata _nameHashes) external view returns (address[] memory);
+interface IFtsoRegistry {
+    function getFtsoBySymbol(string calldata _symbol) external view returns (address);
 }
