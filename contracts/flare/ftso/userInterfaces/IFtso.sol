02be3423230e18c7656f6996fbbde57c33ae7336
MoodyFullWatts <Mockmoody@gmail.com>
2025-06-29 01:22:53 -1000

    Update IFtso.sol
    get current price

--- contracts/flare/ftso/userInterfaces/IFtso.sol
+++ contracts/flare/ftso/userInterfaces/IFtso.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.7.6;
 
 interface IFtso {
     function getCurrentPrice(address _asset) external view returns (uint256, uint256);
-    // Additional FTSO functions can be added as needed
 }
